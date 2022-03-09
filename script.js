89c375284f597808d891bad1daef57baed1ff897
triangletrianglesquareplusx <69000271+triangletrianglesquareplusx@users.noreply.github.com>
2022-03-09 14:47:41 +0200

    testing changes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 'use strict';
-let messageElement = document.querySelector('.message');
-messageElement.textContent = 'you did it';
+/*let messageElement = document.querySelector('.message');
+messageElement.textContent = 'you did it';*/
+let buttonElement = document.querySelector('.check').addEventListener('click',()=>{
+    let guessElement = document.querySelector('.guess');
+    if(guessElement.value == 3){
+        let messageElement = document.querySelector('.message');
+        messageElement.textContent = 'yes!';
+    }
+});
